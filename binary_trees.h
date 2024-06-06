3c3605aaf03dfa3732878491d5011edf8ccd0dbc
ELJRAOUYOMAR <etudecompte78@gmail.com>
2024-06-06 21:20:11 +0100

    Task 20

--- binary_trees.h
+++ binary_trees.h
@@ -39,6 +39,7 @@ int binary_tree_balance(const binary_tree_t *tree);
 int binary_tree_is_perfect(const binary_tree_t *tree);
 binary_tree_t *binary_tree_uncle(binary_tree_t *node);
 void binary_tree_levelorder(const binary_tree_t *tree, void (*func)(int));
+binary_tree_t *binary_tree_rotate_left(binary_tree_t *tree);
 
 void binary_tree_print(const binary_tree_t *tree);
 
