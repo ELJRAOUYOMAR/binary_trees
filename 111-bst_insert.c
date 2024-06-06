4f51635fbc9096ce38150751263b99c28a2bae04
ELJRAOUYOMAR <etudecompte78@gmail.com>
2024-06-06 21:33:03 +0100

    Task 26

---
+++ 111-bst_insert.c
@@ -0,0 +1,53 @@
+#include "binary_trees.h"
+
+/**
+ * bst_insert - Inserts a value in a Binary Search Tree
+ * @tree: Double pointer to the root node of the BST to insert the value
+ * @value: The value to insert
+ *
+ * Return: Pointer to the created node, or NULL on failure
+ */
+bst_t *bst_insert(bst_t **tree, int value)
+{
+    bst_t *current, *new_node;
+
+    if (tree == NULL)
+        return (NULL);
+
+    if (*tree == NULL)
+    {
+        *tree = (bst_t *)binary_tree_node(NULL, value);
+        return (*tree);
+    }
+
+    current = *tree;
+    while (current != NULL)
+    {
+        if (value == current->n)
+            return (NULL); /* Value already exists in the tree */
+        if (value < current->n)
+        {
+            if (current->left == NULL)
+            {
+                new_node = (bst_t *)binary_tree_node(current, value);
+                if (new_node != NULL)
+                    current->left = new_node;
+                return (new_node);
+            }
+            current = current->left;
+        }
+        else
+        {
+            if (current->right == NULL)
+            {
+                new_node = (bst_t *)binary_tree_node(current, value);
+                if (new_node != NULL)
+                    current->right = new_node;
+                return (new_node);
+            }
+            current = current->right;
+        }
+    }
+
+    return (NULL);
+}
--- binary_trees.h
+++ binary_trees.h
@@ -42,6 +42,7 @@ binary_tree_t *binary_tree_uncle(binary_tree_t *node);
 void binary_tree_levelorder(const binary_tree_t *tree, void (*func)(int));
 binary_tree_t *binary_tree_rotate_left(binary_tree_t *tree);
 int binary_tree_is_bst(const binary_tree_t *tree);
+bst_t *array_to_bst(int *array, size_t size);
 
 void binary_tree_print(const binary_tree_t *tree);
 
